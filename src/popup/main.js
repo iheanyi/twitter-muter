09c82d1cb58537cf41418e67998d0540843dbfd6
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2016-12-17 16:18:08 -0500

    Update description and add in image assets.

--- src/popup/main.js
+++ src/popup/main.js
@@ -6,11 +6,11 @@ import 'css-cursors';
 
 require('file?name=_locales/en/messages.json!../_locales/en/messages.json');
 require('file?name=manifest.json!../manifest.json');
-require('file?name=icon-38.png!./images/icon-38.png');
-require('file?name=icon-38.png!./images/icon-38.png');
-require('file?name=icon-19.png!./images/icon-19.png');
-require('file?name=icon-128.png!./images/icon-128.png');
-require('file?name=icon-16.png!./images/icon-16.png');
+require('file?name=icon38.png!./images/icon-38.png');
+require('file?name=icon48.png!./images/icon-48.png');
+require('file?name=icon19.png!./images/icon-19.png');
+require('file?name=icon128.png!./images/icon-128.png');
+require('file?name=icon16.png!./images/icon-16.png');
 
 new Vue({
   store,
